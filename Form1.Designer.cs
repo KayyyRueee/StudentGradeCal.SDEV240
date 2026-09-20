adef6c771e6a1cca21f53f9508e06ce66b9fcaa1
KayyyRueee <khutchinson22@ivytech.edu>
2026-09-20 04:38:41 -0400

    Initial Student Grade Calculator

---
+++ Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace StudentCal
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblStudentID = new Label();
+            txtStudentID = new TextBox();
+            btnLoadClass = new Button();
+            lblClass = new Label();
+            cboClass = new ComboBox();
+            lblAssignments = new Label();
+            dgvAssignments = new DataGridView();
+            colAssignments = new DataGridViewTextBoxColumn();
+            colPosPts = new DataGridViewTextBoxColumn();
+            calGrdPts = new DataGridViewTextBoxColumn();
+            colEstScore = new DataGridViewTextBoxColumn();
+            btnCalculateCurrent = new Button();
+            btnCalculateEstimated = new Button();
+            btnExit = new Button();
+            btnClear = new Button();
+            grpResults = new GroupBox();
+            lblEstimatedLetter = new Label();
+            lblCurrentLetter = new Label();
+            txtEstimatedLetter = new TextBox();
+            txtCurrentLetter = new TextBox();
+            txtEstimatedGrade = new TextBox();
+            txtCurrentGrade = new TextBox();
+            lblEstimatedGrade = new Label();
+            lblCurrentGrade = new Label();
+            lblStatus = new Label();
+            lblStatusMessage = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvAssignments).BeginInit();
+            grpResults.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lblStudentID
+            // 
+            lblStudentID.AutoSize = true;
+            lblStudentID.Location = new Point(17, 26);
+            lblStudentID.Name = "lblStudentID";
+            lblStudentID.Size = new Size(86, 20);
+            lblStudentID.TabIndex = 0;
+            lblStudentID.Text = " Student ID:";
+            // 
+            // txtStudentID
+            // 
+            txtStudentID.Location = new Point(112, 23);
+            txtStudentID.Name = "txtStudentID";
+            txtStudentID.Size = new Size(125, 27);
+            txtStudentID.TabIndex = 1;
+            // 
+            // btnLoadClass
+            // 
+            btnLoadClass.Location = new Point(243, 22);
+            btnLoadClass.Name = "btnLoadClass";
+            btnLoadClass.Size = new Size(115, 29);
+            btnLoadClass.TabIndex = 2;
+            btnLoadClass.Text = "Load Classes";
+            btnLoadClass.UseVisualStyleBackColor = true;
+            btnLoadClass.Click += btnLoadClass_Click;
+            // 
+            // lblClass
+            // 
+            lblClass.AutoSize = true;
+            lblClass.Location = new Point(38, 57);
+            lblClass.Name = "lblClass";
+            lblClass.Size = new Size(42, 20);
+            lblClass.TabIndex = 3;
+            lblClass.Text = "Class";
+            // 
+            // cboClass
+            // 
+            cboClass.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            cboClass.FormattingEnabled = true;
+            cboClass.Location = new Point(112, 54);
+            cboClass.Name = "cboClass";
+            cboClass.Size = new Size(246, 28);
+            cboClass.TabIndex = 4;
+            cboClass.Text = "Select a class";
+            cboClass.SelectedIndexChanged += cboClass_SelectedIndexChanged;
+            // 
+            // lblAssignments
+            // 
+            lblAssignments.AutoSize = true;
+            lblAssignments.Location = new Point(33, 101);
+            lblAssignments.Name = "lblAssignments";
+            lblAssignments.Size = new Size(92, 20);
+            lblAssignments.TabIndex = 5;
+            lblAssignments.Text = "Assignments";
+            lblAssignments.Click += lblAssignments_Click;
+            // 
+            // dgvAssignments
+            // 
+            dgvAssignments.AllowUserToAddRows = false;
+            dgvAssignments.AllowUserToDeleteRows = false;
+            dgvAssignments.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            dgvAssignments.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dgvAssignments.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvAssignments.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvAssignments.Columns.AddRange(new DataGridViewColumn[] { colAssignments, colPosPts, calGrdPts, colEstScore });
+            dgvAssignments.Location = new Point(33, 135);
+            dgvAssignments.MaximumSize = new Size(487, 250);
+            dgvAssignments.MinimumSize = new Size(487, 106);
+            dgvAssignments.Name = "dgvAssignments";
+            dgvAssignments.RowHeadersVisible = false;
+            dgvAssignments.RowHeadersWidth = 51;
+            dgvAssignments.Size = new Size(487, 106);
+            dgvAssignments.TabIndex = 6;
+            // 
+            // colAssignments
+            // 
+            colAssignments.HeaderText = "Assignments";
+            colAssignments.MinimumWidth = 6;
+            colAssignments.Name = "colAssignments";
+            colAssignments.ReadOnly = true;
+            colAssignments.Resizable = DataGridViewTriState.False;
+            colAssignments.Width = 121;
+            // 
+            // colPosPts
+            // 
+            colPosPts.HeaderText = "Possible Pts";
+            colPosPts.MinimumWidth = 6;
+            colPosPts.Name = "colPosPts";
+            colPosPts.ReadOnly = true;
+            colPosPts.Resizable = DataGridViewTriState.False;
+            colPosPts.Width = 114;
+            // 
+            // calGrdPts
+            // 
+            calGrdPts.HeaderText = "Graded Pts";
+            calGrdPts.MinimumWidth = 6;
+            calGrdPts.Name = "calGrdPts";
+            calGrdPts.ReadOnly = true;
+            calGrdPts.Resizable = DataGridViewTriState.False;
+            calGrdPts.Width = 110;
+            // 
+            // colEstScore
+            // 
+            colEstScore.HeaderText = "Estimated Score";
+            colEstScore.MinimumWidth = 6;
+            colEstScore.Name = "colEstScore";
+            colEstScore.Resizable = DataGridViewTriState.False;
+            colEstScore.Width = 145;
+            // 
+            // btnCalculateCurrent
+            // 
+            btnCalculateCurrent.Location = new Point(60, 390);
+            btnCalculateCurrent.Name = "btnCalculateCurrent";
+            btnCalculateCurrent.Size = new Size(124, 50);
+            btnCalculateCurrent.TabIndex = 7;
+            btnCalculateCurrent.Text = "Calculate Current Grade";
+            btnCalculateCurrent.UseVisualStyleBackColor = true;
+            btnCalculateCurrent.Click += btnCalculateCurrent_Click;
+            // 
+            // btnCalculateEstimated
+            // 
+            btnCalculateEstimated.Location = new Point(339, 390);
+            btnCalculateEstimated.Name = "btnCalculateEstimated";
+            btnCalculateEstimated.Size = new Size(127, 50);
+            btnCalculateEstimated.TabIndex = 8;
+            btnCalculateEstimated.Text = "Calculate Estimated Grade";
+            btnCalculateEstimated.UseVisualStyleBackColor = true;
+            btnCalculateEstimated.Click += btnCalculateEstimated_Click;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(355, 455);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(94, 29);
+            btnExit.TabIndex = 9;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(72, 455);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(94, 29);
+            btnClear.TabIndex = 10;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // grpResults
+            // 
+            grpResults.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            grpResults.Controls.Add(lblEstimatedLetter);
+            grpResults.Controls.Add(lblCurrentLetter);
+            grpResults.Controls.Add(txtEstimatedLetter);
+            grpResults.Controls.Add(txtCurrentLetter);
+            grpResults.Controls.Add(txtEstimatedGrade);
+            grpResults.Controls.Add(txtCurrentGrade);
+            grpResults.Controls.Add(lblEstimatedGrade);
+            grpResults.Controls.Add(lblCurrentGrade);
+            grpResults.Location = new Point(38, 503);
+            grpResults.MaximumSize = new Size(482, 160);
+            grpResults.MinimumSize = new Size(482, 116);
+            grpResults.Name = "grpResults";
+            grpResults.Size = new Size(482, 116);
+            grpResults.TabIndex = 11;
+            grpResults.TabStop = false;
+            grpResults.Text = "Results";
+            // 
+            // lblEstimatedLetter
+            // 
+            lblEstimatedLetter.AutoSize = true;
+            lblEstimatedLetter.Location = new Point(220, 69);
+            lblEstimatedLetter.Name = "lblEstimatedLetter";
+            lblEstimatedLetter.Size = new Size(120, 20);
+            lblEstimatedLetter.TabIndex = 19;
+            lblEstimatedLetter.Text = "Estimated Letter:";
+            // 
+            // lblCurrentLetter
+            // 
+            lblCurrentLetter.AutoSize = true;
+            lblCurrentLetter.Location = new Point(238, 32);
+            lblCurrentLetter.Name = "lblCurrentLetter";
+            lblCurrentLetter.Size = new Size(102, 20);
+            lblCurrentLetter.TabIndex = 18;
+            lblCurrentLetter.Text = "Current Letter:";
+            // 
+            // txtEstimatedLetter
+            // 
+            txtEstimatedLetter.Location = new Point(357, 69);
+            txtEstimatedLetter.Name = "txtEstimatedLetter";
+            txtEstimatedLetter.ReadOnly = true;
+            txtEstimatedLetter.Size = new Size(55, 27);
+            txtEstimatedLetter.TabIndex = 17;
+            // 
+            // txtCurrentLetter
+            // 
+            txtCurrentLetter.Location = new Point(357, 32);
+            txtCurrentLetter.Name = "txtCurrentLetter";
+            txtCurrentLetter.ReadOnly = true;
+            txtCurrentLetter.Size = new Size(55, 27);
+            txtCurrentLetter.TabIndex = 16;
+            // 
+            // txtEstimatedGrade
+            // 
+            txtEstimatedGrade.Location = new Point(134, 62);
+            txtEstimatedGrade.Name = "txtEstimatedGrade";
+            txtEstimatedGrade.ReadOnly = true;
+            txtEstimatedGrade.Size = new Size(53, 27);
+            txtEstimatedGrade.TabIndex = 15;
+            // 
+            // txtCurrentGrade
+            // 
+            txtCurrentGrade.Location = new Point(134, 29);
+            txtCurrentGrade.Name = "txtCurrentGrade";
+            txtCurrentGrade.ReadOnly = true;
+            txtCurrentGrade.Size = new Size(51, 27);
+            txtCurrentGrade.TabIndex = 14;
+            // 
+            // lblEstimatedGrade
+            // 
+            lblEstimatedGrade.AutoSize = true;
+            lblEstimatedGrade.Location = new Point(6, 66);
+            lblEstimatedGrade.Name = "lblEstimatedGrade";
+            lblEstimatedGrade.Size = new Size(122, 20);
+            lblEstimatedGrade.TabIndex = 13;
+            lblEstimatedGrade.Text = "Estimated Grade:";
+            // 
+            // lblCurrentGrade
+            // 
+            lblCurrentGrade.AutoSize = true;
+            lblCurrentGrade.Location = new Point(6, 32);
+            lblCurrentGrade.Name = "lblCurrentGrade";
+            lblCurrentGrade.Size = new Size(104, 20);
+            lblCurrentGrade.TabIndex = 12;
+            lblCurrentGrade.Text = "Current Grade:";
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.Location = new Point(38, 667);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(49, 20);
+            lblStatus.TabIndex = 12;
+            lblStatus.Text = "Status";
+            // 
+            // lblStatusMessage
+            // 
+            lblStatusMessage.AutoSize = true;
+            lblStatusMessage.Location = new Point(33, 687);
+            lblStatusMessage.Name = "lblStatusMessage";
+            lblStatusMessage.Size = new Size(65, 20);
+            lblStatusMessage.TabIndex = 13;
+            lblStatusMessage.Text = "              ";
+            lblStatusMessage.Click += lblStatusMessage_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(675, 740);
+            Controls.Add(lblStatusMessage);
+            Controls.Add(lblStatus);
+            Controls.Add(grpResults);
+            Controls.Add(btnClear);
+            Controls.Add(btnExit);
+            Controls.Add(btnCalculateEstimated);
+            Controls.Add(btnCalculateCurrent);
+            Controls.Add(dgvAssignments);
+            Controls.Add(lblAssignments);
+            Controls.Add(cboClass);
+            Controls.Add(lblClass);
+            Controls.Add(btnLoadClass);
+            Controls.Add(txtStudentID);
+            Controls.Add(lblStudentID);
+            Name = "Form1";
+            Text = "Student Grade Calculator";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvAssignments).EndInit();
+            grpResults.ResumeLayout(false);
+            grpResults.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblStudentID;
+        private TextBox txtStudentID;
+        private Button btnLoadClass;
+        private Label lblClass;
+        private ComboBox cboClass;
+        private Label lblAssignments;
+        private DataGridView dgvAssignments;
+        private Button btnCalculateCurrent;
+        private Button btnCalculateEstimated;
+        private Button btnExit;
+        private Button btnClear;
+        private GroupBox grpResults;
+        private Label lblCurrentGrade;
+        private TextBox txtCurrentGrade;
+        private Label lblEstimatedGrade;
+        private Label lblEstimatedLetter;
+        private Label lblCurrentLetter;
+        private TextBox txtEstimatedLetter;
+        private TextBox txtCurrentLetter;
+        private TextBox txtEstimatedGrade;
+        private Label lblStatus;
+        private Label lblStatusMessage;
+        private DataGridViewTextBoxColumn colAssignments;
+        private DataGridViewTextBoxColumn colPosPts;
+        private DataGridViewTextBoxColumn calGrdPts;
+        private DataGridViewTextBoxColumn colEstScore;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,328 @@
+namespace StudentCal
+{
+    public partial class Form1 : Form
+    {
+        private Assignment[] Assignments;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblStatusMessage.Text = "Please enter your student ID.";
+        }
+
+        private void btnLoadClass_Click(object sender, EventArgs e)
+        {
+            if (txtStudentID.Text != "1001" && txtStudentID.Text != "1002")
+            {
+                lblStatusMessage.Text = "Student ID not found.";
+                return;
+            }
+
+            cboClass.Items.Clear();
+            cboClass.Items.Add("Programming");
+            cboClass.Items.Add("Mathematics");
+
+            cboClass.SelectedIndex = -1;
+
+            lblStatusMessage.Text = "Student ID verified. Please select a class.";
+        }
+        private void btnCalculateCurrent_Click(object sender, EventArgs e)
+        {
+            if (Assignments == null)
+            {
+                lblStatusMessage.Text = "Please load a class first.";
+                return;
+            }
+
+            double gradedPoints = 0;
+            double gradedPossible = 0;
+
+            for (int i = 0; i < Assignments.Length; i++)
+            {
+                if (Assignments[i].IsGraded)
+                {
+                    gradedPoints += Assignments[i].EarnedPoints;
+                    gradedPossible += Assignments[i].PossiblePoints;
+                }
+            }
+
+            double currentGrade = CalculateCurrentGrade(
+                gradedPoints,
+                gradedPossible);
+
+            string currentLetterGrade = DetermineLetterGrade(currentGrade);
+
+            txtCurrentGrade.Text = currentGrade.ToString("F2") + "%";
+            txtCurrentLetter.Text = currentLetterGrade;
+
+            lblStatusMessage.Text = "Current grade calculated.";
+        }
+
+        private void btnCalculateEstimated_Click(object sender, EventArgs e)
+        {
+            if (Assignments == null)
+            {
+                lblStatusMessage.Text = "Please load a class first.";
+                return;
+            }
+
+            double gradedPoints = 0;
+            double gradedPossible = 0;
+            double estimatedPoints = 0;
+            double estimatedPossible = 0;
+
+            for (int i = 0; i < Assignments.Length; i++)
+            {
+                if (Assignments[i].IsGraded)
+                {
+                    gradedPoints += Assignments[i].EarnedPoints;
+                    gradedPossible += Assignments[i].PossiblePoints;
+                }
+                else
+                {
+                    string input = dgvAssignments.Rows[i].Cells["colEstScore"].Value?.ToString();
+
+                    if (string.IsNullOrWhiteSpace(input))
+                    {
+                        continue;
+                    }
+
+                    double estimate;
+
+                    if (!double.TryParse(input, out estimate))
+                    {
+                        lblStatusMessage.Text = "Please enter a valid estimated score.";
+                        return;
+                    }
+
+                    if (estimate < 0 || estimate > Assignments[i].PossiblePoints)
+                    {
+                        lblStatusMessage.Text =
+                            "Estimated score must be between 0 and "
+                            + Assignments[i].PossiblePoints + ".";
+                        return;
+                    }
+
+                    estimatedPoints += estimate;
+                    estimatedPossible += Assignments[i].PossiblePoints;
+                }
+            }
+
+            if (estimatedPossible == 0)
+            {
+                lblStatusMessage.Text =
+                    "Enter an estimated score before calculating.";
+                return;
+            }
+
+            double estimatedGrade = CalculateEstimatedGrade(
+                gradedPoints,
+                gradedPossible,
+                estimatedPoints,
+                estimatedPossible);
+
+            string estimatedLetterGrade =
+                DetermineLetterGrade(estimatedGrade);
+
+            txtEstimatedGrade.Text = estimatedGrade.ToString("F2") + "%";
+            txtEstimatedLetter.Text = estimatedLetterGrade;
+
+            lblStatusMessage.Text = "Estimated grade calculated.";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtStudentID.Clear();
+
+            cboClass.SelectedIndex = -1;
+            cboClass.Text = "Select a class";
+
+            dgvAssignments.Rows.Clear();
+
+            txtCurrentGrade.Clear();
+            txtCurrentLetter.Clear();
+            txtEstimatedGrade.Clear();
+            txtEstimatedLetter.Clear();
+
+            Assignments = null;
+
+            lblStatusMessage.Text = "Ready";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+        static Assignment[] GetAssignments(string classStudy)
+        {
+            Assignment[] assignments;
+
+            if (classStudy == "Programming")
+            {
+                assignments = new Assignment[]
+                {
+                    new Assignment
+                    {
+                        Name = "Assignment 1",
+                        PossiblePoints = 100,
+                        EarnedPoints = 90,
+                        IsGraded = true
+                    },
+                    new Assignment
+                    {
+                        Name = "Assignment 2",
+                        PossiblePoints = 100,
+                        EarnedPoints = 85,
+                        IsGraded = true
+                    },
+                    new Assignment
+                    {
+                        Name = "Final Project",
+                        PossiblePoints = 100,
+                        EarnedPoints = 0,
+                        IsGraded = false
+                    }
+                };
+            }
+            else
+            {
+                assignments = new Assignment[]
+                {
+                    new Assignment
+                    {
+                        Name = "Test 1",
+                        PossiblePoints = 100,
+                        EarnedPoints = 80,
+                        IsGraded = true
+                    },
+                    new Assignment
+                    {
+                        Name = "Test 2",
+                        PossiblePoints = 100,
+                        EarnedPoints = 90,
+                        IsGraded = true
+                    },
+                    new Assignment
+                    {
+                        Name = "Final Test",
+                        PossiblePoints = 100,
+                        EarnedPoints = 0,
+                        IsGraded = false
+                    }
+                };
+            }
+
+            return assignments;
+        }
+
+        static double CalculateCurrentGrade(double studentInput, double totalPoints)
+        {
+
+            if (totalPoints <= 0)
+            {
+                return 0;
+            }
+
+            double currentGrade = (studentInput / totalPoints) * 100;
+            return currentGrade;
+        }
+
+        static double CalculateEstimatedGrade(
+            double gradedPoints,
+            double gradedPossible,
+            double estimatedPoints,
+            double estimatedPossible)
+        {
+            double totalPoints = gradedPoints + estimatedPoints;
+            double totalPossible = gradedPossible + estimatedPossible;
+
+            if (totalPossible <= 0)
+            {
+                return 0;
+            }
+
+            double estimatedGrade = (totalPoints / totalPossible) * 100;
+            return estimatedGrade;
+        }
+
+        static string DetermineLetterGrade(double percentage)
+        {
+            if (percentage >= 90) return "A";
+            else if (percentage >= 80) return "B";
+            else if (percentage >= 70) return "C";
+            else if (percentage >= 60) return "D";
+            else return "F";
+        }
+
+        private void cboClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboClass.SelectedItem == null)
+            {
+                return;
+            }
+
+            string classStudy = cboClass.SelectedItem.ToString();
+
+            Assignments = GetAssignments(classStudy);
+
+            dgvAssignments.Rows.Clear();
+
+            foreach (Assignment assignment in Assignments)
+            {
+                if (assignment.IsGraded)
+                {
+                    dgvAssignments.Rows.Add(
+                        assignment.Name,
+                        assignment.PossiblePoints,
+                        assignment.EarnedPoints,
+                        "");
+                }
+                else
+                {
+                    dgvAssignments.Rows.Add(
+                        assignment.Name,
+                        assignment.PossiblePoints,
+                        "",
+                        "");
+                }
+            }
+
+            lblStatusMessage.Text = classStudy + " loaded successfully.";
+        }
+
+        private void lblAssignments_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStatusMessage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStatusMessage_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    class Assignment
+    {
+        public string Name;
+        public double PossiblePoints;
+        public double EarnedPoints;
+        public bool IsGraded;
+    }
+}
